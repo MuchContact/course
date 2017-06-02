82cb926d51f38840d6dbacd10f971d23c8417d77
MuchContact <liuhengjiangis@foxmail.com>
2017-06-02 13:19:18 +0800

    add scenario 2

---
+++ zabbix/chk_remote_mysql.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+# FileName:    chk_mysql.sh
+# Revision:    1.0
+# Date:        2017/05/22
+# Description: 
+# Notes:       ~
+# -------------------------------------------------------------------------------
+# param
+# MYSQL_CONN="mysqladmin -h${host} -P${Port} -u${u} -p${p}"
+MYSQL_CONN="mysqladmin --login-path=${2}"
+case $1 in 
+    Uptime) 
+        result=`${MYSQL_CONN} status|cut -f2 -d":"|cut -f1 -d"T"` 
+        echo $result 
+        ;; 
+    Com_update) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_update"|cut -d"|" -f3`
+        echo $result 
+        ;; 
+    Slow_queries) 
+        result=`${MYSQL_CONN} status |cut -f5 -d":"|cut -f1 -d"O"`
+        echo $result 
+        ;; 
+    Com_select) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_select"|cut -d"|" -f3`
+        echo $result 
+                ;; 
+    Com_rollback) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_rollback"|cut -d"|" -f3` 
+                echo $result 
+                ;; 
+    Questions) 
+        result=`${MYSQL_CONN} status|cut -f4 -d":"|cut -f1 -d"S"`
+                echo $result 
+                ;; 
+    Com_insert) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_insert"|cut -d"|" -f3`
+                echo $result 
+                ;; 
+    Com_delete) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_delete"|cut -d"|" -f3` 
+                echo $result 
+                ;; 
+    Com_commit) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_commit"|cut -d"|" -f3` 
+                echo $result 
+                ;; 
+    Bytes_sent) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Bytes_sent" |cut -d"|" -f3`
+                echo $result 
+                ;; 
+    Bytes_received) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Bytes_received" |cut -d"|" -f3` 
+                echo $result 
+                ;; 
+    Com_begin) 
+        result=`${MYSQL_CONN} extended-status |grep -w "Com_begin"|cut -d"|" -f3`
+                echo $result 
+                ;;
+    Version)
+        result=`${MYSQL_CONN} version |grep -w "version"|cut -b 17- `
+                echo $result
+                ;;
+    Ping)
+        result=`${MYSQL_CONN} ping |grep -c alive`
+                echo $result
+                ;;
+	*)
+        echo "Usage:$0(Uptime|Com_update|Slow_queries|Com_select|Com_rollback|Questions|Com_insert|Com_delete|Com_commit|Bytes_sent|Bytes_received|Com_begin)" 
+        ;; 
+esac
